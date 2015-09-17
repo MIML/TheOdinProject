a75113c5eed7e4b77a259dac9fd9ad13d5f4c6c3
MIML <y.nice8@gmail.com>
2015-09-17 22:34:54 +1000

    set up CSS and basic JS

---
+++ Carousel/assets/script.js
@@ -0,0 +1,18 @@
+﻿(function ($) {
+    var $prev = $('.prev'), $next = $('.next');
+
+    $(document).ready(function () {
+        $next.click(function () {
+            console.log("next");
+            $( "#slider > ul" ).animate({left: '-800px'});
+        });
+        $prev.click(function () {
+            console.log("prev");
+             $( "#slider > ul" ).animate({left: '0px'});
+        });
+    });
+
+    function init() {
+        alert("hi");
+    }
+})(jQuery);
