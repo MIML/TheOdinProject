75ce367fa48daa201980ff3286ca46e5e71762e4
MIML <y.nice8@gmail.com>
2015-09-30 17:10:29 +1000

    Update script.js

--- Carousel/assets/script.js
+++ Carousel/assets/script.js
@@ -1,18 +1,36 @@
 (function ($) {
-    var $prev = $('.prev'), $next = $('.next');
+    var $prev = $('.prev'), $next = $('.next'), $current;
+    var sectionFrom, $slide = $('.img'), $slideActive = $('.img.active'), $navLink = $('#nav a');
 
-    $(document).ready(function () {
+    init();
+    slide();
+
+    function slide() {
+        $current = $slideActive.index();
+
         $next.click(function () {
-            console.log("next");
-            $( "#slider > ul" ).animate({left: '-800px'});
+            $current++;
+            if ($current > 2) {
+                $current = 0;
+            }
+            $("#slider > ul").animate({ left: '-' + ($current * 800) + 'px' });
+            console.log($current);
         });
         $prev.click(function () {
-            console.log("prev");
-             $( "#slider > ul" ).animate({left: '0px'});
+            $current--;
+            if ($current < 0) {
+                $current = 2;
+            }
+            $("#slider > ul").animate({ left: '-' + ($current * 800) + 'px' });
         });
+    }
+
+    $navLink.on('click', function (e) {
+        $('#nav li .active').remove();
+        this.parent().addClass('active');
     });
 
     function init() {
-        alert("hi");
+        $("#slider > ul").animate({ left: '0px' });
     }
 })(jQuery);
