36695c5de26a5ce96227538eb5efaf5ccfad87e6
MIML <y.nice8@gmail.com>
2015-09-30 19:51:20 +1000

    Working solution

--- Carousel/assets/script.js
+++ Carousel/assets/script.js
@@ -1,18 +1,40 @@
 ﻿(function ($) {
-    var $prev = $('.prev'), $next = $('.next');
+    var $prev = $('.prev'), $next = $('.next'), $current;
+    var sectionFrom, $slide = $('.img'), $slideActive = $('.img.active'), $navLink = $('#nav li');
 
-    $(document).ready(function () {
+    init();
+    slide();
+
+    function animate(){
+        $("#slider > ul").animate({ left: '-' + ($current * 800) + 'px' });
+    }
+
+
+    function slide() {
+        $current = $slideActive.index();
+
         $next.click(function () {
-            console.log("next");
-            $( "#slider > ul" ).animate({left: '-800px'});
+            $current++;
+            if ($current > 2) {
+                $current = 0;
+            }
+            animate();
         });
         $prev.click(function () {
-            console.log("prev");
-             $( "#slider > ul" ).animate({left: '0px'});
+            $current--;
+            if ($current < 0) {
+                $current = 2;
+            }
+            animate();
         });
+    }
+
+    $navLink.on('click', function (e) {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
     });
 
     function init() {
-        alert("hi");
+        $("#slider > ul").animate({ left: '0px' });
     }
 })(jQuery);
